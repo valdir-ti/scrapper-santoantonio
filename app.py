3ba684ca61103d95f28f59ae1554d40faade6594
Valdir Silva <valdir.ti@gmail.com>
2025-01-09 21:01:44 -0300

    refact: create get images function

--- app.py
+++ app.py
@@ -10,16 +10,35 @@ BASE_URL = "https://www.lojasantoantonio.com.br/"
 BASE_URL_PRODUCT = "https://www.lojasantoantonio.com.br/cake-box-quad-crist-ctpa-2l-165x165x8cm-6233-lsc-toys/p"
 # BASE_URL_PRODUCT = "https://www.lojasantoantonio.com.br/chiclete-mentos-pure-fresh-sabor-morango-56g---van-melle-100855/p"
 
+def get_images(driver):
+    # Capturar o contêiner das imagens
+    swiper_wrapper = WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.CLASS_NAME, "swiper-wrapper"))
+    )       
+    swiper_slides = swiper_wrapper.find_elements(By.CLASS_NAME, "swiper-slide")
+
+    # Criar uma lista para armazenar os links das imagens
+    image_links = []
+
+    # Iterar por cada slide para capturar as imagens
+    for slide in swiper_slides:
+        try:
+            # Localizar a tag <img> dentro do slide
+            img = slide.find_element(By.TAG_NAME, "img")
+            # Capturar o atributo 'src'
+            img_src = img.get_attribute("src")
+            # Adicionar o link à lista
+            image_links.append(img_src)
+        except Exception as e:
+            print(f"Erro ao capturar imagem no slide: {e}")        
+    return image_links
+    
 def main(): 
     driver = webdriver.Chrome()
     try:
         # Abrir a página
         driver.get(BASE_URL_PRODUCT)
         sleep(5)
-        
-        swiper_wrapper = WebDriverWait(driver, 10).until(
-            EC.presence_of_element_located((By.CLASS_NAME, "swiper-wrapper"))
-        )
 
         script_tags = driver.find_elements("xpath", "//script[@type='application/ld+json']")
 
@@ -31,40 +50,8 @@ def main():
             # Capturar o conteúdo JSON da segunda tag (Categoria)
             json_categoria = script_tags[1].get_attribute("innerHTML")
 
-            ### Buscar as imagens do produto
-            container = driver.find_element(By.XPATH, "//div[@data-testid='thumbnail-swiper']")        
-            
-            # Capturar todas as tags <figure> dentro do contêiner
-            figures = container.find_elements(By.TAG_NAME, "figure")
-
-            # Criar um array para armazenar os links das imagens
-            image_links = []
-
-            for figure in figures:
-                # Localizar a tag <img> dentro da <figure>
-                img = figure.find_element(By.TAG_NAME, "img")
-                img_src = img.get_attribute("src")  # Capturar o atributo 'src' da tag <img>
-                
-                # Adicionar o link ao array
-                image_links.append(img_src)
-                
-            # Capturar as divs filhas diretamente
-            swiper_slides = swiper_wrapper.find_elements(By.CLASS_NAME, "swiper-slide")
-
-            # Criar uma lista para armazenar os links das imagens
-            image_links = []
-
-            # Iterar por cada slide para capturar as imagens
-            for slide in swiper_slides:
-                try:
-                    # Localizar a tag <img> dentro do slide
-                    img = slide.find_element(By.TAG_NAME, "img")
-                    # Capturar o atributo 'src'
-                    img_src = img.get_attribute("src")
-                    # Adicionar o link à lista
-                    image_links.append(img_src)
-                except Exception as e:
-                    print(f"Erro ao capturar imagem no slide: {e}")
+            # Capturar os links das imagens
+            image_links = get_images(driver)
 
             try:
                 # Converter os conteúdos para objetos Python
